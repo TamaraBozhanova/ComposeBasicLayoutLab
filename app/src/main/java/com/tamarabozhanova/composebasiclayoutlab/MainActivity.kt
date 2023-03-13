4154e31ecb23bfb866e4599206c1714047ec2656
Tamara Bozhanova <tamara.bozhanova@gmail.com>
2023-03-13 00:22:31 -0700

    final review

--- app/src/main/java/com/tamarabozhanova/composebasiclayoutlab/MainActivity.kt
+++ app/src/main/java/com/tamarabozhanova/composebasiclayoutlab/MainActivity.kt
@@ -36,15 +36,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBasicLayoutLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ComposeBasicLayoutLabApp()
         }
     }
 }
@@ -320,19 +312,6 @@ fun AlignYourBodyRowPreview(){
 fun SearchBarPreview() {
     ComposeBasicLayoutLabTheme {
         SearchBar(Modifier.padding(8.dp))
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeBasicLayoutLabTheme {
-        Greeting("Android")
     }
 }
 
